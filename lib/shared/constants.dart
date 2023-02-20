74c37727110201d653c1951661a2381a8a39bc4f
123sufiyan <assufiyan33@gmail.com>
2023-02-20 22:22:16 +0530

    all initial errors solved

---
+++ lib/shared/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  static String appId = '1:509712332776:web:242b0617c0205ad4bdd65d';
+  static String apiKey = 'AIzaSyAJd-B6VJia4agRuWjXA1KlIHsVfWUmaDw';
+  static String messageSenderId = '509712332776';
+  static String projectId = 'chatappflutter-26baa';
+}
